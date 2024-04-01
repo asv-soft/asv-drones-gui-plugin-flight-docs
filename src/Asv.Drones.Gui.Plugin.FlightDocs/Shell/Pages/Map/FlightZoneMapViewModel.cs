ce8c3a261a3c22b8274e6f3f82d5785cc04f5464
Eugene <u03@asv.me>
2024-04-01 13:46:36 +0300

    feat: correct notification display conditions
    Asana: https://app.asana.com/0/1203851531040615/1205622126571077/f

--- src/Asv.Drones.Gui.Plugin.FlightDocs/Shell/Pages/Map/FlightZoneMapViewModel.cs
+++ src/Asv.Drones.Gui.Plugin.FlightDocs/Shell/Pages/Map/FlightZoneMapViewModel.cs
@@ -33,8 +33,8 @@ public class FlightZoneMapViewModelConfig
 {
     public double Zoom { get; set; }
     public GeoPoint MapCenter { get; set; }
-    public List<FlightZonePoint> FlightZoneAnchors { get; set; } = new ();
-    public List<TakeOffLandPoint> TakeOffLandAnchors { get; set; } = new ();
+    public List<FlightZonePoint> FlightZoneAnchors { get; set; } = new();
+    public List<TakeOffLandPoint> TakeOffLandAnchors { get; set; } = new();
 }
 
 [ExportShellPage(UriString)]
@@ -52,7 +52,6 @@ public class FlightZoneMapViewModel : MapPageViewModel, IFlightZoneMap
     {
         if (Design.IsDesignMode)
         {
-            
         }
     }
 
@@ -62,38 +61,43 @@ public class FlightZoneMapViewModel : MapPageViewModel, IFlightZoneMap
         [ImportMany(UriString)] IEnumerable<IViewModelProvider<IMapMenuItem>> exportedMenuItems,
         [ImportMany(UriString)] IEnumerable<IViewModelProvider<IMapAnchor>> markers,
         [ImportMany(UriString)] IEnumerable<IViewModelProvider<IMapWidget>> widgets,
-        [ImportMany(UriString)] IEnumerable<IViewModelProvider<IMapAction>> actions) : base(Uri, map, status, exportedMenuItems, markers, widgets, actions)
+        [ImportMany(UriString)] IEnumerable<IViewModelProvider<IMapAction>> actions) : base(Uri, map, status,
+        exportedMenuItems, markers, widgets, actions)
     {
         _cfg = cfg;
         _flightConfig = cfg.Get<FlightZoneMapViewModelConfig>();
         Zoom = _flightConfig.Zoom is 0 ? 1 : _flightConfig.Zoom;
         Center = _flightConfig.MapCenter;
 
+
         for (var i = 0; i < _flightConfig.FlightZoneAnchors.Count; i++)
         {
-            _flightZoneAnchors.Add(new FlightZoneAnchor(Guid.NewGuid().ToString(), _flightConfig.FlightZoneAnchors[i].Location, i, loc) {Name = _flightConfig.FlightZoneAnchors[i].Name});
+            _flightZoneAnchors.Add(
+                new FlightZoneAnchor(Guid.NewGuid().ToString(), _flightConfig.FlightZoneAnchors[i].Location, i, loc)
+                    { Name = _flightConfig.FlightZoneAnchors[i].Name });
         }
-        
+
         foreach (var t in _flightConfig.TakeOffLandAnchors)
         {
-            _takeOffLandAnchors.Add(new TakeOffLandAnchor(Guid.NewGuid().ToString(), t.Location, t.TakeOffLand, loc) {Name = t.Name});
+            _takeOffLandAnchors.Add(new TakeOffLandAnchor(Guid.NewGuid().ToString(), t.Location, t.TakeOffLand, loc)
+                { Name = t.Name });
         }
-        
+
         _flightZoneAnchors.Add(new FlightZonePolygon());
-        
+
         foreach (var provider in markers)
         {
             if (provider is FlightZoneMapAnchorProvider flightZoneProvider)
             {
                 flightZoneProvider.Update(_flightZoneAnchors);
             }
-            
+
             if (provider is TakeOffLandMapAnchorProvider takeOffLandProvider)
             {
                 takeOffLandProvider.Update(_takeOffLandAnchors);
             }
         }
-        
+
         this.WhenPropertyChanged(vm => vm.Zoom)
             .Subscribe(_ =>
             {
@@ -102,7 +106,7 @@ public class FlightZoneMapViewModel : MapPageViewModel, IFlightZoneMap
                 cfg.Set(_flightConfig);
             })
             .DisposeItWith(Disposable);
-            
+
         this.WhenPropertyChanged(vm => vm.Center)
             .Subscribe(_ =>
             {
@@ -115,25 +119,30 @@ public class FlightZoneMapViewModel : MapPageViewModel, IFlightZoneMap
 
     public void SaveToCfg()
     {
-        _flightConfig.FlightZoneAnchors = new ();
+        _flightConfig.FlightZoneAnchors = new();
         foreach (var anchor in FlightZoneAnchors.Items)
         {
             if (anchor is FlightZoneAnchor flightZoneAnchor)
             {
-                _flightConfig.FlightZoneAnchors.Add(new FlightZonePoint() {Location = flightZoneAnchor.Location, Name = flightZoneAnchor.Name});
+                _flightConfig.FlightZoneAnchors.Add(new FlightZonePoint()
+                    { Location = flightZoneAnchor.Location, Name = flightZoneAnchor.Name });
             }
         }
-            
-        _flightConfig.TakeOffLandAnchors = new ();
+
+        _flightConfig.TakeOffLandAnchors = new();
         foreach (var anchor in TakeOffLandAnchors.Items)
         {
             var takeOffLandAnchor = (TakeOffLandAnchor)anchor;
-            _flightConfig.TakeOffLandAnchors.Add(new TakeOffLandPoint() {Location = takeOffLandAnchor.Location, Name = takeOffLandAnchor.Name, TakeOffLand = takeOffLandAnchor.TakeOffLand});
+            _flightConfig.TakeOffLandAnchors.Add(new TakeOffLandPoint()
+            {
+                Location = takeOffLandAnchor.Location, Name = takeOffLandAnchor.Name,
+                TakeOffLand = takeOffLandAnchor.TakeOffLand
+            });
         }
         _cfg.Set(_flightConfig);
         IsChanged = false;
     }
-    
+
     public override async Task<bool> TryClose()
     {
         if (!IsChanged) return true;
@@ -161,9 +170,9 @@ public class FlightZoneMapViewModel : MapPageViewModel, IFlightZoneMap
                 return true;
         }
     }
-    
+
     [Reactive]
-    public  bool IsChanged { get; set; }
+    public bool IsChanged { get; set; }
     [Reactive]
     public bool IsChangeSave { get; set; }
     public SourceList<IMapAnchor> FlightZoneAnchors => _flightZoneAnchors;
